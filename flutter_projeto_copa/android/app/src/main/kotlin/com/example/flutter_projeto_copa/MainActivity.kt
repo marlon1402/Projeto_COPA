8c172df995c6610e4faa981345e68bbc1e2d69e0
Marlon Samuel <marlonsamuel9333@gmail.com>
2024-08-20 16:43:15 -0300

    Update commit

---
+++ flutter_projeto_copa/android/app/src/main/kotlin/com/example/flutter_projeto_copa/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_projeto_copa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ flutter_projeto_copa/lib/main.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter_projeto_copa/homePage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      routes: {
+        '/':(context) => HomePage(),
+      },
+    );
+  }
+}
